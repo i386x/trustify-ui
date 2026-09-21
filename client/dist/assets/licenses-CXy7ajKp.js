d3db0ad917b51b87fddff746919673027965eb42
i386x <i386x@users.noreply.>
2026-09-21 23:40:17 +0000

    Checkin dist dir

---
+++ client/dist/assets/licenses-CXy7ajKp.js
@@ -0,0 +1,1 @@
+import{Fn as e,a as t}from"./useBranding-RHCQqHa4.js";import{t as n,w as r}from"./apiInit-AALgruS4.js";var i=`licenses`,a=(a={},o=!1)=>{let{data:s,isLoading:c,error:l,refetch:u}=e({queryKey:[i,a],queryFn:()=>r({client:n,query:{...t(a)}}),enabled:!o});return{result:{data:s?.data?.items||[],total:s?.data?.total??0,params:a},isFetching:c,fetchError:l,refetch:u}};export{a as t};
